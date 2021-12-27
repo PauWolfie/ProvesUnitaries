a273f6754e0e3618378e2574817661752fed26eb
Pau Llobet <paullobet99@gmail.com>
2021-12-27 17:52:12 +0100

    Classes and methods defined (Data)

--- src/data/DocPath.java
+++ src/data/DocPath.java
@@ -1,4 +1,38 @@
 package data;
 
+import javax.print.Doc;
+
+/**
+ * Essential data classes
+ */
 public class DocPath {
+    // The accreditation number of the SS
+    private final String path;
+
+    public DocPath(String Docpath) {
+        this.path = Docpath;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DocPath pathToComp = (DocPath) o;
+        return path.equals(pathToComp.path);
+    }
+
+    @Override
+    public int hashCode() {
+        return path.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Path of the document{" + "Dirección='" + path + '\'' + '}';
+    }
+
 }
--- src/data/PINcode.java
+++ src/data/PINcode.java
@@ -1,4 +1,35 @@
 package data;
 
-public class PINcode {
+/**
+ * Essential data classes
+ */
+final public class PINcode {
+    // The PIN sent to autenticate
+    private final String PINcode;
+
+    public PINcode(int PIN) {
+        this.PINcode = String.valueOf(PIN);
+    }
+
+    public String getPINcode() {
+        return PINcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PINcode PINcodeToComp = (PINcode) o;
+        return PINcode.equals(PINcodeToComp.PINcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return PINcode.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "PIN code{" + "Código PIN='" + PINcode + '\'' + '}';
+    }
 }
