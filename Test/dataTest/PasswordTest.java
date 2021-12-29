8f5ad8f21bdd534c6e8ed535c2b4f2079f3eac7d
Pau Llobet <paullobet99@gmail.com>
2021-12-29 19:29:01 +0100

    PasswordTest added

---
+++ Test/dataTest/PasswordTest.java
@@ -0,0 +1,26 @@
+package dataTest;
+
+import exceptions.EmptyException;
+import exceptions.WrongFormedException;
+
+public class PasswordTest implements dataTestInterface {
+    @Override
+    public void correctTest() throws WrongFormedException, EmptyException {
+
+    }
+
+    @Override
+    public void emptyTest() {
+
+    }
+
+    @Override
+    public void wrongFormatTest() {
+
+    }
+
+    @Override
+    public void compareTest() throws WrongFormedException, EmptyException {
+
+    }
+}
