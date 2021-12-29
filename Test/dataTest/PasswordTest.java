1ac5556685d1e571c7ef1bc1520eeb9f5e870220
Pau Llobet <paullobet99@gmail.com>
2021-12-29 19:30:28 +0100

    PasswordTest modified

--- Test/dataTest/PasswordTest.java
+++ Test/dataTest/PasswordTest.java
@@ -2,25 +2,22 @@ package dataTest;
 
 import exceptions.EmptyException;
 import exceptions.WrongFormedException;
+import jdk.jshell.spi.ExecutionControl;
 
 public class PasswordTest implements dataTestInterface {
     @Override
     public void correctTest() throws WrongFormedException, EmptyException {
-
     }
 
     @Override
     public void emptyTest() {
-
     }
 
     @Override
     public void wrongFormatTest() {
-
     }
 
     @Override
     public void compareTest() throws WrongFormedException, EmptyException {
-
     }
 }
